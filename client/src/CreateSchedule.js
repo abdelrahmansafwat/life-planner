b9e55a35fc0c8880b999138325cf88a8857eead4
Abdelrahman M. Safwat <33398534+abdelrahmansafwat@users.noreply.github.com>
2021-12-01 12:36:45 +0200

    Black text in choices

--- client/src/CreateSchedule.js
+++ client/src/CreateSchedule.js
@@ -36,9 +36,25 @@ import { Carousel } from "react-responsive-carousel";
 import "./styles.css";
 
 let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let dayColors = ["#38483c","#a2c3aa","#7fa387","#719178","#637f69","#556d5a","#475b4b",];
+let dayColors = [
+  "#38483c",
+  "#a2c3aa",
+  "#7fa387",
+  "#719178",
+  "#637f69",
+  "#556d5a",
+  "#475b4b",
+];
 //let dayColors = ["#8FB4CF","#CF5384","#CF7730","#CFAC30","#D1394E","#D1C23F","#30CCBD",];
-let dayEventColors = ["#8db596","#d1e1d5","#c6dacb","#bbd3c0","#afcbb6","#a4c4ab","#98bca1",];
+let dayEventColors = [
+  "#8db596",
+  "#d1e1d5",
+  "#c6dacb",
+  "#bbd3c0",
+  "#afcbb6",
+  "#a4c4ab",
+  "#98bca1",
+];
 //let dayEventColors = ["#A0CAE8","#F6629D","#F58C38","#F0C837","#F04158","#F0DE47","#3AF2E0",];
 
 export default function CreateSchedule() {
@@ -203,6 +219,7 @@ export default function CreateSchedule() {
               value={eventType}
               label="Age"
               onChange={(e) => setEventType(e.target.value)}
+              style={{ color: "black" }}
             >
               <MenuItem value={"Sports"}>Sports</MenuItem>
               <MenuItem value={"Class"}>Class</MenuItem>
@@ -251,7 +268,12 @@ export default function CreateSchedule() {
                   >
                     <Avatar
                       variant="rounded"
-                      style={{ backgroundColor: "#8DB596", marginBottom: 25, width: 500, height: 50 }}
+                      style={{
+                        backgroundColor: "#8DB596",
+                        marginBottom: 25,
+                        width: 500,
+                        height: 50,
+                      }}
                     >
                       <Typography variant="h3">Life Planner</Typography>
                     </Avatar>
